a73929208a58df30b508d563c217247c94cbfafb
Pauline <pauline.anna.havez@gmail.com>
2026-05-16 18:36:24 +0200

    remove flyway dependency, add sql script and pg admin image

---
+++ init.sql
@@ -0,0 +1,69 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE MERCHANT(
+                         merchant_id UUID DEFAULT gen_random_uuid(),
+                         phone_number VARCHAR(15) NOT NULL,
+                         headquarters_address VARCHAR(255) NOT NULL,
+                         siret_number VARCHAR(14),
+                         email VARCHAR(255) NOT NULL,
+                         PRIMARY KEY(merchant_id),
+                         UNIQUE(phone_number),
+                         UNIQUE(siret_number),
+                         UNIQUE(email)
+);
+
+CREATE TABLE SERVICE(
+                        service_id UUID DEFAULT gen_random_uuid(),
+                        name VARCHAR(50) NOT NULL,
+                        merchant_id UUID NOT NULL,
+                        PRIMARY KEY(service_id),
+                        FOREIGN KEY(merchant_id) REFERENCES MERCHANT(merchant_id)
+);
+
+CREATE TABLE CUSTOMER(
+                         customer_id UUID DEFAULT gen_random_uuid(),
+                         name VARCHAR(255) NOT NULL,
+                         phone_number VARCHAR(15) NOT NULL,
+                         email VARCHAR(255) NOT NULL,
+                         merchant_id UUID NOT NULL,
+                         PRIMARY KEY(customer_id),
+                         UNIQUE(phone_number),
+                         UNIQUE(email),
+                         FOREIGN KEY(merchant_id) REFERENCES MERCHANT(merchant_id)
+);
+
+CREATE TABLE APPOINTMENT(
+                            appointment_id UUID DEFAULT gen_random_uuid(),
+                            appointment_date TIMESTAMP NOT NULL,
+                            status SMALLINT NOT NULL,
+                            earned_points SMALLINT,
+                            service_id UUID NOT NULL,
+                            merchant_id UUID NOT NULL,
+                            customer_id UUID NOT NULL,
+                            PRIMARY KEY(appointment_id),
+                            FOREIGN KEY(service_id) REFERENCES SERVICE(service_id),
+                            FOREIGN KEY(merchant_id) REFERENCES MERCHANT(merchant_id),
+                            FOREIGN KEY(customer_id) REFERENCES CUSTOMER(customer_id)
+);
+
+CREATE TABLE TRANSACTION(
+                            transaction_id UUID DEFAULT gen_random_uuid(),
+                            transaction_date TIMESTAMP NOT NULL,
+                            earned_points INT,
+                            service_id UUID NOT NULL,
+                            merchant_id UUID NOT NULL,
+                            customer_id UUID NOT NULL,
+                            PRIMARY KEY(transaction_id),
+                            FOREIGN KEY(service_id) REFERENCES SERVICE(service_id),
+                            FOREIGN KEY(merchant_id) REFERENCES MERCHANT(merchant_id),
+                            FOREIGN KEY(customer_id) REFERENCES CUSTOMER(customer_id)
+);
+
+-- Data test : Marie Dupont, merchant
+INSERT INTO MERCHANT (phone_number, headquarters_address, siret_number, email)
+VALUES (
+           '060000000',
+           '12 rue du Nil Paris 75002',
+           '80295478500015',
+           'marie.dupont@chezmarie.fr'
+       );
